a5b6ba1167aefa129e35de2108dd0407660bf936
Muhammad Hatta Putra <hatta.putraaa@gmail.com>
2018-12-19 14:22:27 +0700

    Update twitter_consume.py

--- twitter_consume.py
+++ twitter_consume.py
@@ -5,10 +5,10 @@ from tweepy import OAuthHandler
 
 def get_twitter_auth():
     try:
-        consumer_key = "Fz2TC10tUiL4xB3oYOoca0eD4"
-        consumer_secret = "B3TKlgIjgYDfz1AW7vbC1hYVuHq55RQ8aNQFd4zs8M3HcJgMnM"
-        access_token = "1074527011949826048-DgZlg9Hz7TWJYVJ6PRXaysoB4G5lwN"
-        access_secret = "9376Qy5M6o815QEzUmxGi3dU3edVBHW5pWryplD0XFNrz"
+        consumer_key = "XXX"
+        consumer_secret = "XXX"
+        access_token = "XXX"
+        access_secret = "XXX"
     except KeyError:
         sys.stderr.write("TWITTER_* environment variables not set\n")
         sys.exit(1)
